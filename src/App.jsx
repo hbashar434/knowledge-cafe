29182b632efaf5da067e7d420a2535f287bfcd80
hbashar434 <h.bashar434@gmail.com>
2023-03-31 02:41:20 +0600

    upto data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Blog from "./components/Blog/Blog";
+import Bookmark from "./components/Bookmark/Bookmark";
 import Header from "./components/Header/Header";
 
 const App = () => {
+  const [blogs, setBlog] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => setBlog(data));
+  }, []);
+
   return (
-    <div className=" my-8 mx-20">
+    <div className=" my-8 p-4 md:mx-20">
       <Header></Header>
       <hr className="mt-3" />
-      
+      <div className="md:flex justify-between mt-6">
+        <div>
+          {blogs.map((blog) => (
+            <Blog blog={blog} key={blog.id}></Blog>
+          ))}
+        </div>
+        <div>
+          <Bookmark></Bookmark>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 
-const Blog = () => {
+const Blog = ({ blog }) => {
+  const { id, cover, image, name, title, date, time } = blog;
+  console.log(id);
+
   return (
-    <div>
-      <img src="" alt="" />
-      <div>
-        <img src="" alt="" />
-        <div>
-          <h4>name</h4>
-          <p>date</p>
+    <div className="card mt-8 w-full bg-base-100">
+      <figure className=" rounded-2xl">
+        <img src={cover} alt="banner-image" className=" w-full h-96" />
+      </figure>
+      <div className="card-body">
+        <div className=" flex justify-between">
+          <div className="flex">
+            <figure className=" w-12 h-12 rounded-full mr-4">
+              <img src={image} alt="author-image" />
+            </figure>
+            <div>
+              <h3 className=" font-bold">{name}</h3>
+              <p className=" text-xs font-semibold">{date}</p>
+            </div>
+          </div>
+          <div>
+            <p>{time} min read</p>
+          </div>
         </div>
-        <p>time: </p>
+        <h2 className="card-title font-bold">{title}</h2>
+        <p>#beginners #programming</p>
+        <div className="card-actions">
+          <button className=" btn-link">Mark as read</button>
+        </div>
       </div>
-      <h2>title</h2>
-      <p>#beginners #programming</p>
-      <button className="btn btn-link">Mark as read</button>
+      <hr />
     </div>
   );
 };
