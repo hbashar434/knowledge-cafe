ae096d99869c72776438f18b1383af920756359a
hbashar434 <h.bashar434@gmail.com>
2023-03-31 02:50:38 +0600

    upto blog ui

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Bookmark = () => {
-  return <div>Bookmark</div>;
+  return (
+    <div className="card bg-gray-300 w-96 rounded-lg p-4">
+      <h3 className="text-lg font-bold text-blue-900">Spent time on read : </h3>
+      <div className="mt-10">
+        <h1>Bookmarked Blog : </h1>
+        <h2 className="card p-4 bg-white rounded-md">Bookmark : </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Bookmark;
